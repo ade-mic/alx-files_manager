724af894ecbe51f239affa6f677633ab4b2b2dfe
ade-mic <ademic.aina@gmail.com>
2024-12-08 04:35:27 +0100

    fix controllers/AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,16 +6,16 @@ class AppController {
     res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    })
+    });
   }
 
   static async getStats(req, res) {
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
     res.status(200).json({
-      users: users,
-      files: files,
-    })
+      users,
+      files,
+    });
   }
 }
 
