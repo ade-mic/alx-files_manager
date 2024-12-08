a66f669b7658cb26ab8099ced82429f109c4526c
ade-mic <ademic.aina@gmail.com>
2024-12-08 20:12:33 +0100

    Usercontroller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,32 +1,43 @@
-import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import sha1 from 'sha1';
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
 
+    // Check if email is provided
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
 
+    // Check if password is provided
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
 
     try {
+      // Check if the email already exists in the database
       const existingUser = await dbClient.db.collection('users').findOne({ email });
       if (existingUser) {
         return res.status(400).json({ error: 'Already exist' });
       }
-      const hashedPassword = sha1(password);
-      const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
 
+      // Hash the password
+      const hashedPassword = sha1(password);
+
+      // Create the new user in the database
+      const result = await dbClient.db.collection('users').insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      // Respond with the created user's email and id
       return res.status(201).json({
         id: result.insertedId,
         email,
       });
     } catch (error) {
-      console.error('Error creatinf user:', error);
+      console.error('Error creating user:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
