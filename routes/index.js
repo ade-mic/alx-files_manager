79c0f6e6bf71ff073e1df7a113425624ad836b47
ade-mic <ademic.aina@gmail.com>
2024-12-08 04:05:34 +0100

    2. First API

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { Router } from 'express';
+import { appendFile } from 'fs';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stat', AppController.getStats);
---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from 'routes/index.js';
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use('/', routes)
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
